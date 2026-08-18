f8aad99e38a1cfdcf0c5a3ccc76178626a271a9e
Felipe Marcelino <felipe.marcelino1991@gmail.com>
2026-08-18 01:42:25 -0300

    flake annd shelll

---
+++ shell.nix
@@ -0,0 +1,30 @@
+{
+  pkgs,
+  lib,
+  stdenv,
+  ...
+}:
+let
+  pkgs-unfree = import pkgs.path {
+    inherit (pkgs) system;
+    config.allowUnfree = true;
+  };
+  pythonPackages = pkgs-unfree.python313Packages;
+in
+pkgs-unfree.mkShell {
+  buildInputs = [
+    pythonPackages.python
+    pythonPackages.venvShellHook
+    pkgs-unfree.autoPatchelfHook
+
+  ];
+  venvDir = "./.venv";
+  postVenvCreation = ''
+
+    unset SOURCE_DATE_EPOCH    
+    autoPatchelf ./.venv   '';
+  postShellHook = ''
+
+    unset SOURCE_DATE_EPOCH     
+    export LD_LIBRARY_PATH=${lib.makeLibraryPath [ stdenv.cc.cc ]}:$LD_LIBRARY_PATH '';
+}
